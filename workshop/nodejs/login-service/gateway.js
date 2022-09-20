99e8c5a3cdd3e39f88a5c766a7af3fd31c594a98
up1 <somkiat.p@gmail.com>
2022-09-21 00:03:17 +0700

    Component test for login service with nock

--- workshop/nodejs/login-service/gateway.js
+++ workshop/nodejs/login-service/gateway.js
@@ -1,9 +1,13 @@
-const axios = require('axios');
+const axios = require("axios");
 
 function UserService(api_server_url) {
   this.AXIOS = axios.create({ baseURL: api_server_url });
   this.getUserById = (id) => {
-    return this.AXIOS.get(`/users/${id}`).then((res) => res.data);
+    return this.AXIOS.get(`/users/${id}`)
+      .then((res) => res.data)
+      .catch((error) => {
+        return { code: 404 }
+      });
   };
 }
 module.exports = UserService;
---
+++ workshop/nodejs/login-service/tests/login-component.test.js
@@ -0,0 +1,24 @@
+const UserService = require("../gateway");
+const nock = require("nock");
+
+const URL = "http://127.0.0.1:4000";
+
+test("Success to get user by id from user server", async () => {
+  const mockedUser = {
+    user: "somkiat",
+    name: "Somkiat Pui",
+    role: "admin",
+  };
+  nock(URL).get("/users/1").reply(200, mockedUser);
+  const service = new UserService(URL);
+  const response = await service.getUserById(1);
+  expect(response).toEqual(mockedUser);
+});
+
+test("Failure to get user by id from user server :: user not found", async () => {
+  const expectedError = {code: 404};
+  nock(URL).get("/users/2").reply(404);
+  const service = new UserService(URL);
+  const response = await service.getUserById(2);
+  expect(response).toEqual(expectedError);
+});
