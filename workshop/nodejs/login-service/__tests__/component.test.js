c668f583b2ad3a9f3ba2b9da316003ff5759d73d
up1 <somkiat.p@gmail.com>
2022-09-22 10:41:30 +0700

    Use nock to mock api

--- workshop/nodejs/login-service/__tests__/component.test.js
+++ workshop/nodejs/login-service/__tests__/component.test.js
@@ -1,6 +1,12 @@
+const nock = require('nock')
 const UserService = require("../gateway");
 
 test("Success case with Get user by id = 1", async() => {
+
+  nock('http://localhost:4000')
+  .get('/users/1')
+  .reply(200, {"name": "Somkiat Pui", "role": "admin", "user": "somkiat"})
+
   const service = new UserService("http://localhost:4000");
   const acutualResult = await service.getUserById(1);
   expect(acutualResult).toEqual({"name": "Somkiat Pui", "role": "admin", "user": "somkiat"});
