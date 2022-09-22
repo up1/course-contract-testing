c0a49cf6562f00bf19f668deda7d9e169b862d3f
up1 <somkiat.p@gmail.com>
2022-09-22 12:03:38 +0700

    Add fail case into contract test

--- workshop/nodejs/login-service/__tests__/contract.test.js
+++ workshop/nodejs/login-service/__tests__/contract.test.js
@@ -51,3 +51,29 @@ test("Success to get user detail", async () => {
     );
   });
 }); 
+
+test("Failure :: User not found", async () => {
+  // set up Pact interactions
+  await provider.addInteraction({
+    states: [{ description: "user not found" }],
+    uponReceiving: "get user by id = 2",
+    withRequest: {
+      method: "GET",
+      path: "/users/2",
+    },
+    willRespondWith: {
+      status: 404
+    },
+  });
+
+  await provider.executeTest(async (mockService) => {
+    const service = new UserService(mockService.url);
+    // make request to Pact mock server
+    const response = await service.getUserById(2);
+    expect(response).toStrictEqual(
+      {
+        error: "User not found"
+      }
+    );
+  });
+}); 
