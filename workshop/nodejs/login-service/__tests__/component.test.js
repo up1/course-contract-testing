8c307c0a40760c380012feb6e9cb301122823cfc
up1 <somkiat.p@gmail.com>
2022-09-22 10:48:53 +0700

    Add failure case

--- workshop/nodejs/login-service/__tests__/component.test.js
+++ workshop/nodejs/login-service/__tests__/component.test.js
@@ -11,3 +11,14 @@ test("Success case with Get user by id = 1", async() => {
   const acutualResult = await service.getUserById(1);
   expect(acutualResult).toEqual({"name": "Somkiat Pui", "role": "admin", "user": "somkiat"});
 });
+
+test("Failure case with Get user by id = 2 => User not found", async() => {
+
+  nock('http://localhost:4000')
+  .get('/users/2')
+  .reply(404, {})
+
+  const service = new UserService("http://localhost:4000");
+  const acutualResult = await service.getUserById(2);
+  expect(acutualResult).toEqual({error: "User not found"});
+});
--- workshop/nodejs/login-service/gateway.js
+++ workshop/nodejs/login-service/gateway.js
@@ -3,7 +3,9 @@ const axios = require('axios');
 function UserService(api_server_url) {
   this.AXIOS = axios.create({ baseURL: api_server_url });
   this.getUserById = (id) => {
-    return this.AXIOS.get(`/users/${id}`).then((res) => res.data);
+    return this.AXIOS.get(`/users/${id}`)
+    .then((res) => res.data)
+    .catch( e =>  { return {error: "User not found"} });
   };
 }
 module.exports = UserService;
